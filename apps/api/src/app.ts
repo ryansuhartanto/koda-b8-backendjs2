d72a68d70016b4ad27853ab6bd9a9c610874baa0
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-29 10:59:33 +0700

    refactor(api): rename directory

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import type { Express } from "express";
 
-import { corsMiddleware } from "#/middleware/cors";
+import { corsMiddleware } from "#/middlewares/cors";
 
 const app: Express = express();
 
