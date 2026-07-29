28c461b8db4561f5db0406a1b68b14710961895d
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-07-29 10:59:03 +0700

    chore: fix absolute import rule

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+import { globSync } from "node:fs";
+
 import oxfmt from "@kekkon-nexus/config/oxfmt";
 import oxlint from "@kekkon-nexus/config/oxlint";
 import { defineConfig } from "vite-plus";
+
+import pkg from "#/package.json" with { type: "json" };
 
 export default defineConfig({
 	fmt: {
@@ -24,9 +28,21 @@ export default defineConfig({
 			"vite-plus/prefer-vite-plus-imports": "error",
 			"no-relative-import-paths/no-relative-import-paths": [
 				"warn",
-				{ allowSameFolder: false, rootDir: "src", prefix: "#" },
+				{ allowSameFolder: false, rootDir: `/src`, prefix: "#" },
 			],
 		},
+
+		overrides: pkg.workspaces
+			.flatMap((glob) => globSync(glob))
+			.map((dir) => ({
+				files: [`${dir}/**`],
+				rules: {
+					"no-relative-import-paths/no-relative-import-paths": [
+						"warn",
+						{ allowSameFolder: false, rootDir: `${dir}/src`, prefix: "#" },
+					],
+				},
+			})),
 	},
 	staged: {
 		"*": "vp check --fix --no-error-on-unmatched-pattern",
