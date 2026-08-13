0b2b4f76d41b93e530e2c1f34dad1137741174a8
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-08-13 15:34:41 +0700

    build: support pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import oxfmt from "@kekkon-nexus/config/oxfmt";
 import oxlint from "@kekkon-nexus/config/oxlint";
 import { defineConfig } from "vite-plus";
 
-import pkg from "#/package.json" with { type: "json" };
-
 export default defineConfig({
 	fmt: {
 		...oxfmt,
@@ -43,21 +41,19 @@ export default defineConfig({
 
 		ignorePatterns: ["apps/web/src/components/ui"],
 
-		overrides: pkg.workspaces
-			.flatMap((glob) => globSync(glob))
-			.map((dir) => ({
-				files: [`${dir}/**`],
-				rules: {
-					"no-relative-import-paths/no-relative-import-paths": [
-						"warn",
-						{
-							allowSameFolder: false,
-							rootDir: `${dir}/src`,
-							prefix: "#",
-						},
-					],
-				},
-			})),
+		overrides: globSync("apps/*").map((dir) => ({
+			files: [`${dir}/**`],
+			rules: {
+				"no-relative-import-paths/no-relative-import-paths": [
+					"warn",
+					{
+						allowSameFolder: false,
+						rootDir: `${dir}/src`,
+						prefix: "#",
+					},
+				],
+			},
+		})),
 	},
 	staged: {
 		"*": "vp check --fix --no-error-on-unmatched-pattern",
